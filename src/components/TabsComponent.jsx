339e1d6a52b37e92ee2d885487532d033405a827
Burak <burak@90pixel.com>
2022-01-12 16:30:04 +0300

    Initialize demo app.

--- src/components/TabsComponent.jsx
+++ src/components/TabsComponent.jsx
@@ -1,21 +1,6 @@
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 
 function TabsComponent() {
-  return (
-    <Tabs>
-      <TabList>
-        <Tab>Title 1</Tab>
-        <Tab>Title 2</Tab>
-      </TabList>
-
-      <TabPanel>
-        <h2>Any content 1</h2>
-      </TabPanel>
-      <TabPanel>
-        <h2>Any content 2</h2>
-      </TabPanel>
-    </Tabs>
-  );
+  return <div>xx</div>;
 }
 export default TabsComponent;
